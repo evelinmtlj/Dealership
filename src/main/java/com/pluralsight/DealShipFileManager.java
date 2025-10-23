eb130f19961607f0be49ead9d459b4ceb1691f2a
jesicamatul <evelinmtlj@gmail.com>
2025-10-23 15:26:45 -0400

    created method that loads vehicles from file

--- src/main/java/com/pluralsight/DealShipFileManager.java
+++ src/main/java/com/pluralsight/DealShipFileManager.java
@@ -1,6 +1,55 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class DealShipFileManager {
     //method that reads from file
+
+    public static ArrayList<Vehicle> vehicles() {
+
+        ArrayList<Vehicle > vehicles = new ArrayList<>();
+
+        try {
+            FileReader fileReader = new FileReader("inventory.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                //split the file by parts
+                String[] parts = line.split("\\|");
+
+                int vin = Integer.parseInt(parts[0]);
+                int year = Integer.parseInt(parts[1]);
+                String make = parts[2];
+                String model = parts[3];
+                String vehicleType = parts[4];
+                String color = parts[5];
+                int odometer = Integer.parseInt(parts[6]);
+                double price = Double.parseDouble(parts[7]);
+
+                // add this
+                Vehicle vehicle= new Vehicle(vin,year,make,model,vehicleType,color,odometer,price);
+                vehicles.add(vehicle);
+
+            }
+            bufferedReader.close();
+            fileReader.close();
+
+        } catch (IOException e) {
+            System.out.println("Transaction file not found! ");
+
+        }
+
+        return vehicles;
+    }
+
+
 
 }
