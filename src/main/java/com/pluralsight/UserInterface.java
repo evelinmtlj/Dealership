b955266ace963d69cf589d05f0b9dd95a5ad7300
jesicamatul <evelinmtlj@gmail.com>
2025-10-25 13:18:44 -0400

    added messages if vehicles not found

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -55,10 +55,10 @@ public class UserInterface {
                     getByColorRequest();
                     break;
                 case 5:
-                    //method
+                   getByMileageRequest();
                     break;
                 case 6:
-                    //method
+                    getByVehicleTypeRequest();
                     break;
                 case 7:
                     getAllVehiclesRequest();
@@ -85,10 +85,15 @@ public class UserInterface {
 // step 1 : get price range from user min,max
         double minPrice = ConsoleHelper.promptForFloat("Enter minimum price you would like to see ");
         double maxPrice= ConsoleHelper.promptForFloat("Enter maximum price you would like to see");
+        boolean found = false;
         //step 2: loop thru inventory, for each vehicle get their price
         for(Vehicle v: dealership.getAllVehicles()) {
             if(v.getPrice() >= minPrice && v.getPrice() <= maxPrice) {
                 System.out.println(v);
+                found = true;
+            }
+            if(!found) {
+                System.out.println("No vehicles found with price range of: " + minPrice + " or " + maxPrice);
             }
         }
 
@@ -97,10 +102,15 @@ public class UserInterface {
     private  void getByMakeModel(){
 String make = ConsoleHelper.promptForString("Enter make you would like to see");
 String model = ConsoleHelper.promptForString("Enter model you would like to see");
+boolean found = false;
 
 for(Vehicle v: dealership.getAllVehicles()) {
     if(v.getMake().equalsIgnoreCase(make) && v.getModel().equalsIgnoreCase(model)) {
         System.out.println(v);
+        found = true;
+    }
+    if(!found) {
+        System.out.println("No vehicle found for " + make + " " + model);
     }
 }
     }
@@ -108,37 +118,78 @@ for(Vehicle v: dealership.getAllVehicles()) {
     private  void getByYearRequest() {
         int minYear = ConsoleHelper.promptForInt("Enter minimum year");
         int maxYear = ConsoleHelper.promptForInt("Enter max year");
+        boolean found = false;
 
         //loop thru vehicles
         for (Vehicle v : dealership.getAllVehicles()) {
             if (v.getYear() >= minYear && v.getYear() <= maxYear) {
                 System.out.println(v);
+                found = true;
             }
-
+            if(!found) {
+                System.out.println("No vehicle found for year "+ minYear + "or " + maxYear);
+            }
         }
     }
         private void getByColorRequest () {
             String color = ConsoleHelper.promptForString("Enter color you would like to see");
+            boolean found = false;
             for (Vehicle v : dealership.getAllVehicles()) {
                 if (v.getColor().toLowerCase().contains(color)) {
                     System.out.println(v);
+                    found = true;
+                }
+                if(!found) {
+                    System.out.println("No vehicle found with color " + color);
+                }
+            }
+
+
+        }
+
+        private  void getByMileageRequest () {
+        int minMileage = ConsoleHelper.promptForInt("Enter minimum year");
+        int maxMileage = ConsoleHelper.promptForInt("Enter maximum year");
+        boolean found = false;
+
+        //loop thru the vehicles
+            for(Vehicle v: dealership.getAllVehicles()) {
+                if(v.getOdometer() >= minMileage && v.getOdometer() <= maxMileage) {
+                    System.out.println(v);
+                    found = true;
+                }
+                if(!found) {
+                    System.out.println("No vehicles found with mileage of "+ minMileage + "or " + maxMileage);
                 }
             }
 
         }
 
-        private static void getByMileageRequest () {
-
-        }
-
-        private static void getByVehicleTypeRequest () {
+        private  void getByVehicleTypeRequest () {
+        String type = ConsoleHelper.promptForString("Enter the type of vehicle you would like to see");
+        boolean found = false;
+        //loop
+            for(Vehicle v: dealership.getAllVehicles()) {
+                if(v.getVehicleType().equalsIgnoreCase(type)) {
+                    System.out.println(v);
+                    found= true;
+                }
+                if(!found) {
+                    System.out.println("No vehicles found for the type of " + type);
+                }
+            }
 
         }
 
         private void getAllVehiclesRequest () {
             System.out.println("------- Displaying all vehicles -------- ");
+            boolean found = false;
             for (Vehicle v : dealership.getAllVehicles()) {
                 System.out.println(v);
+                found = true;
+            }
+            if(!found) {
+                System.out.println("No vehicles currently");
             }
 
         }
