e866a11421f8034c5b9aed9b274fda1b3b719ebb
jesicamatul <evelinmtlj@gmail.com>
2025-10-30 09:34:08 -0400

    formula for monthly payments

--- src/main/java/com/pluralsight/SalesContract.java
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -9,7 +9,7 @@ public class SalesContract extends Contract {
 
     public SalesContract(String dateOfContract, String customerName, String customerEmail, String vehicleSold,   boolean finance, double vehiclePrice) {
         super(dateOfContract, customerName, customerEmail, vehicleSold);
-        this.finance = finance;
+        this.finance = false;
         this.vehiclePrice = vehiclePrice;
     }
 
@@ -52,9 +52,44 @@ public class SalesContract extends Contract {
 return vehiclePrice + getSalesTaxAmount() + getRecordingFee() + getProcessingFee();
     }
 
+    public double formulaMonthlyPayment(double principal,double annualRate,int months){
+
+        //convert annual interest rate to monthly decimal rate
+double monthlyInterestRate = (annualRate /12) ;
+
+
+        //calculate EMI
+        double payment  =
+                principal * monthlyInterestRate * Math.pow(1 +  monthlyInterestRate,months) /
+                (Math.pow(1 + monthlyInterestRate, months)-1);
+
+
+       return payment;
+    }
+
     @Override
     public double getMonthlyPayment(){
+        //check if customer did not finance
 
+        if(finance == false){
+            return 0;
+        }
+
+ // determine loan terms
+        double annualRate;
+        int months;
+
+        if(vehiclePrice >= 10000){
+            annualRate = .0425;
+            months = 48;
+        } else {
+            annualRate = .0525;
+            months = 24;
+        }
+        //get pricipal price also total price
+        double principal = getTotalPrice();
+
+        return formulaMonthlyPayment(principal,annualRate,months);
     }
 
 }
