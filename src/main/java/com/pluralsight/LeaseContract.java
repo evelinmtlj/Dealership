fb699ddb780558b88d1a1f6640d211a9388b4f82
jesicamatul <evelinmtlj@gmail.com>
2025-10-30 10:02:02 -0400

    finished lease contract class

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,4 +1,61 @@
 package com.pluralsight;
 
-public class LeaseContract {
+public class LeaseContract extends Contract {
+    private double expectedEndingValue;
+    private double leaseFee;
+    private double vehiclePrice;
+
+
+    public LeaseContract(String dateOfContract, String customerName, String customerEmail, String vehicleSold, double vehiclePrice) {
+        super(dateOfContract, customerName, customerEmail, vehicleSold);
+        this.vehiclePrice = vehiclePrice;
+    }
+ //getters and setters for vehicle price
+    public double getVehiclePrice() {
+        return vehiclePrice;
+    }
+
+    public void setVehiclePrice(double vehiclePrice) {
+        this.vehiclePrice = vehiclePrice;
+    }
+    //getters
+
+    public double getExpectedEndingValue() {
+        return vehiclePrice * 0.5;
+    }
+
+    public double getLeaseFee() {
+        return vehiclePrice * .07;
+    }
+
+    //formula
+    public double formulaMonthlyPayment(double principal,double annualRate,int months){
+
+        //convert annual interest rate to monthly decimal rate
+        double monthlyInterestRate = (annualRate /12) ;
+
+
+        //calculate EMI
+        double payment  =
+                principal * monthlyInterestRate * Math.pow(1 +  monthlyInterestRate,months) /
+                        (Math.pow(1 + monthlyInterestRate, months)-1);
+
+
+        return payment;
+    }
+
+    @Override
+    public double getTotalPrice(){
+return vehiclePrice + getLeaseFee()  - getExpectedEndingValue();
+    }
+
+    @Override
+    public double getMonthlyPayment(){
+double principal = getTotalPrice();
+double annualRate = .04;
+int months = 36;
+
+return formulaMonthlyPayment(principal,annualRate,months);
+
+    }
 }
