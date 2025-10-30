4bbe18ed6dab84c794df4c39e268fbc370c809d5
jesicamatul <evelinmtlj@gmail.com>
2025-10-30 10:28:24 -0400

    testing

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,11 +6,26 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Dealership dealership = DealShipFileManager.getDealership();
-        //created an instance and call its display method
-     UserInterface uInterface = new UserInterface(dealership);
+//        Dealership dealership = DealShipFileManager.getDealership();
+//        //created an instance and call its display method
+//     UserInterface uInterface = new UserInterface(dealership);
+//
+//     uInterface.display();
 
-     uInterface.display();
+        SalesContract sales = new SalesContract("10-21-2023","Jesica mtl","matuljesica@gmail.com","Toyota camry",true,12500);
+
+        sales.setFinance(true);
+        System.out.println(sales);
+
+
+
+        LeaseContract lease = new LeaseContract("12-21-2021","Evelin ne","evelinttk@gmail.com","Honda civic",18000);
+        System.out.println("\n " + lease);
+
+//        System.out.println("----------------- Sales contract------------");
+//        System.out.println("Vehicle price: $" + sales.getVehiclePrice());
+//        System.out.println("Total price: $" + sales.getTotalPrice());
+//        System.out.println("Monthly payment: $" + sales.getMonthlyPayment());
 
     }
 }
